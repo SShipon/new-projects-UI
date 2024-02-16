e9c17927daad4bd19af613d76c4004ef954edc91
sabrina <sabrinaahmednitu@gmail.com>
2024-02-16 21:09:21 +0600

    welcome left done

--- src/components/Home/Welcome.jsx
+++ src/components/Home/Welcome.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import card1 from '../../assets/home/Card-1.png';
-import card2 from '../../assets/home/Card-2.png';
+import card1 from '../../assets/home/Card-1.png.png';
+import card2 from '../../assets/home/Card-2.png.png';
+import './Welcome.css';
 
 const Welcome = () => {
-    return (
-       <div>
-        <div className="flex justify-center items-center">
-          <div className="welcome-box-left">
+  return (
+    <div>
+      <div className="welcome-container">
+        <div className="welcome-box-left">
+          <div>
+            <h1>Welcome to X</h1>
+          </div>
+          <div>
             <img src={card1} alt="" />
           </div>
-          <div className="lorem-box-right">
-            <img src={card2} alt="" />
-          </div>
+        </div>
+        <div className="lorem-box-right">
+          <img src={card2} alt="" />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Welcome;
-
